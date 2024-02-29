6fc1982b2f15209078c78583d3ed99fdf7acffb3
kovtonyuk <kovtonyuk@A315-23>
2024-02-29 15:34:16 +0200

    Add project files.

---
+++ MyForm.h
@@ -0,0 +1,357 @@
+#pragma once
+const int n = 2, m = 3;
+int a[n][m], i, j;
+
+namespace Project2 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	protected:
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::DataGridView^ dataGridView2;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column2;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column3;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column4;
+	private: System::Windows::Forms::Panel^ panel1;
+	private: System::Windows::Forms::CheckBox^ checkBox3;
+	private: System::Windows::Forms::CheckBox^ checkBox2;
+	private: System::Windows::Forms::CheckBox^ checkBox1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->Column1 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->dataGridView2 = (gcnew System::Windows::Forms::DataGridView());
+			this->Column2 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Column3 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Column4 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->checkBox3 = (gcnew System::Windows::Forms::CheckBox());
+			this->checkBox2 = (gcnew System::Windows::Forms::CheckBox());
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView2))->BeginInit();
+			this->panel1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->AllowUserToAddRows = false;
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->ColumnHeadersVisible = false;
+			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(1) { this->Column1 });
+			this->dataGridView1->Location = System::Drawing::Point(48, 0);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->RowHeadersVisible = false;
+			this->dataGridView1->RowHeadersWidth = 62;
+			this->dataGridView1->RowTemplate->Height = 28;
+			this->dataGridView1->Size = System::Drawing::Size(160, 453);
+			this->dataGridView1->TabIndex = 0;
+			// 
+			// Column1
+			// 
+			this->Column1->HeaderText = L"Column1";
+			this->Column1->MinimumWidth = 8;
+			this->Column1->Name = L"Column1";
+			this->Column1->Width = 150;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(243, 160);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(157, 90);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(305, 280);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(0, 20);
+			this->label1->TabIndex = 2;
+			// 
+			// dataGridView2
+			// 
+			this->dataGridView2->AllowUserToAddRows = false;
+			this->dataGridView2->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView2->ColumnHeadersVisible = false;
+			this->dataGridView2->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(3) {
+				this->Column2,
+					this->Column3, this->Column4
+			});
+			this->dataGridView2->Location = System::Drawing::Point(471, 15);
+			this->dataGridView2->Name = L"dataGridView2";
+			this->dataGridView2->RowHeadersVisible = false;
+			this->dataGridView2->RowHeadersWidth = 62;
+			this->dataGridView2->RowTemplate->Height = 28;
+			this->dataGridView2->Size = System::Drawing::Size(473, 235);
+			this->dataGridView2->TabIndex = 3;
+			// 
+			// Column2
+			// 
+			this->Column2->HeaderText = L"Column2";
+			this->Column2->MinimumWidth = 8;
+			this->Column2->Name = L"Column2";
+			this->Column2->Width = 150;
+			// 
+			// Column3
+			// 
+			this->Column3->HeaderText = L"Column3";
+			this->Column3->MinimumWidth = 8;
+			this->Column3->Name = L"Column3";
+			this->Column3->Width = 150;
+			// 
+			// Column4
+			// 
+			this->Column4->HeaderText = L"Column4";
+			this->Column4->MinimumWidth = 8;
+			this->Column4->Name = L"Column4";
+			this->Column4->Width = 150;
+			// 
+			// panel1
+			// 
+			this->panel1->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->panel1->Controls->Add(this->checkBox3);
+			this->panel1->Controls->Add(this->checkBox2);
+			this->panel1->Controls->Add(this->checkBox1);
+			this->panel1->Location = System::Drawing::Point(508, 280);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(284, 138);
+			this->panel1->TabIndex = 4;
+			// 
+			// checkBox3
+			// 
+			this->checkBox3->AutoSize = true;
+			this->checkBox3->Location = System::Drawing::Point(41, 96);
+			this->checkBox3->Name = L"checkBox3";
+			this->checkBox3->Size = System::Drawing::Size(73, 24);
+			this->checkBox3->TabIndex = 2;
+			this->checkBox3->Text = L"Сума";
+			this->checkBox3->UseVisualStyleBackColor = true;
+			this->checkBox3->CheckedChanged += gcnew System::EventHandler(this, &MyForm::checkBox3_CheckedChanged);
+			// 
+			// checkBox2
+			// 
+			this->checkBox2->AutoSize = true;
+			this->checkBox2->Location = System::Drawing::Point(41, 55);
+			this->checkBox2->Name = L"checkBox2";
+			this->checkBox2->Size = System::Drawing::Size(129, 24);
+			this->checkBox2->TabIndex = 1;
+			this->checkBox2->Text = L"Мінімальний";
+			this->checkBox2->UseVisualStyleBackColor = true;
+			this->checkBox2->CheckedChanged += gcnew System::EventHandler(this, &MyForm::checkBox2_CheckedChanged);
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->AutoSize = true;
+			this->checkBox1->Location = System::Drawing::Point(41, 13);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(148, 24);
+			this->checkBox1->TabIndex = 0;
+			this->checkBox1->Text = L"Максимальний";
+			this->checkBox1->UseVisualStyleBackColor = true;
+			this->checkBox1->CheckedChanged += gcnew System::EventHandler(this, &MyForm::checkBox1_CheckedChanged);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(619, 439);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(268, 61);
+			this->button2->TabIndex = 5;
+			this->button2->Text = L"button2";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(820, 293);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 26);
+			this->textBox1->TabIndex = 6;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(820, 337);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(100, 26);
+			this->textBox2->TabIndex = 7;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(820, 378);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(100, 26);
+			this->textBox3->TabIndex = 8;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1143, 571);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->panel1);
+			this->Controls->Add(this->dataGridView2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->dataGridView1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView2))->EndInit();
+			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+		
+private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+		dataGridView1->Rows->Add(10);
+		dataGridView2->Rows->Add(3);
+		
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		int const n = 10; int i, s = 0; int mas[n]; float sr;
+		Random^ rnd = gcnew Random();
+		for (i = 0; i < n; i++)
+			dataGridView1->Rows[i]->Cells[0]->Value = Convert::ToString(rnd->Next(-50, 50));
+		for (i = 0; i < n; i++)
+		{
+			mas[i] = Convert::ToInt32(dataGridView1->Rows[i]->Cells[0]->Value);
+			s += mas[i];
+		}
+		sr = (float)s / n;
+		label1->Text = Convert::ToString(sr);
+
+	}
+
+private: System::Void checkBox1_CheckedChanged(System::Object^ sender, System::EventArgs^ e) {
+	for (int i=0;i<n;i++)
+		for (int j = 0; j < m; j++)
+		{
+			a[i][j] = Convert::ToInt32(dataGridView2[j, i]->Value);
+		}
+	int maxs = a[0][0];
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			if (checkBox1->Checked == true)
+			{
+				{
+					if (a[i][j] > maxs)
+						maxs = a[i][j];
+				}
+
+			}
+	textBox1 ->Text = maxs.ToString();
+}
+private: System::Void checkBox2_CheckedChanged(System::Object^ sender, System::EventArgs^ e) {
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+		{
+			a[i][j] = Convert::ToInt32(dataGridView2[j, i]->Value);
+		}
+	int mins = a[0][0];
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			if (checkBox2->Checked == true)
+			{
+				{
+					if (a[i][j] < mins)
+						mins = a[i][j];
+				}
+
+			}
+	textBox2->Text = mins.ToString();
+}
+private: System::Void checkBox3_CheckedChanged(System::Object^ sender, System::EventArgs^ e) {
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+		{
+			a[i][j] = Convert::ToInt32(dataGridView2[j, i]->Value);
+		}
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			if (checkBox3->Checked == true)
+			{
+				{
+					sum += a[i][j];
+				}
+
+			}
+	textBox3->Text = sum.ToString();
+}
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+
+	Random^ rnd = gcnew Random();
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+		{
+			dataGridView2[j, i]->Value = Convert::ToString(rnd->Next(50));
+		}
+
+}
+};
+}
